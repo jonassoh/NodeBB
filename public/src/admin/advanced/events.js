c5847f3039bd5281fe2511de86c49e5d414a6aad
Jonas <jonass@andrew.cmu.edu>
2025-01-22 15:15:41 -0500

    revert changes

--- public/src/admin/advanced/events.js
+++ public/src/admin/advanced/events.js
@@ -4,26 +4,21 @@
 define('admin/advanced/events', ['bootbox', 'alerts', 'autocomplete'], function (bootbox, alerts, autocomplete) {
 	const Events = {};
 
-	function handleDeleteAllEvents(err) {
-		if (err) {
-			return alerts.error(err);
-		}
-		$('.events-list').empty();
-	}
-	function handleConfirm(confirm) {
-		if (confirm) {
-			socket.emit('admin.deleteAllEvents', handleDeleteAllEvents(err));
-		}
-	}
-	function handleClick() {
-		bootbox.confirm('[[admin/advanced/events:confirm-delete-all-events]]', handleConfirm(confirm));
-		return false;
-	}
-
 	Events.init = function () {
-		
-		$('[data-action="clear"]').on('click', handleClick());
-		
+		$('[data-action="clear"]').on('click', function () {
+			bootbox.confirm('[[admin/advanced/events:confirm-delete-all-events]]', (confirm) => {
+				if (confirm) {
+					socket.emit('admin.deleteAllEvents', function (err) {
+						if (err) {
+							return alerts.error(err);
+						}
+						$('.events-list').empty();
+					});
+				}
+			});
+			return false;
+		});
+
 		$('.delete-event').on('click', function () {
 			const $parentEl = $(this).parents('[data-eid]');
 			const eid = $parentEl.attr('data-eid');
